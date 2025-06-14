bfdbcd42df91113a13f6ff4a9eb5b3690ca416d3
Martin Fulop <hi@martinfulop.dev>
2025-06-14 17:22:15 +0200

    replace cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://8yuzjbsqfa.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://1hah82ebje.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ii9w8ee2jk.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
